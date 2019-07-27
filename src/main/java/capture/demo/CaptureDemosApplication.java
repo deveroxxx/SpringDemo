50efd97cbe1a279e5239c88be90bf1a00980ce92
Bakos János <devero@indamail.hu>
2019-07-27 14:30:48 +0200

    @Add base project layout

---
+++ src/main/java/capture/demo/CaptureDemosApplication.java
@@ -0,0 +1,13 @@
+package capture.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureDemosApplication.class, args);
+	}
+
+}
